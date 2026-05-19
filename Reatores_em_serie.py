d965b7b6e891c785e354428b56bb28d75836d389
Julio Dutra <61989878+juliosdutra@users.noreply.github.com>
2026-05-19 10:53:21 -0300

    Implement CSTR model and simulation functions

---
+++ Reatores_em_serie.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class ParametrosCSTR:
+  vazao = 100.0 : float
+  volume = 50.0 : float
+  constante = 0.5: float
+
+def modelo_cstr(CAin, params):
+  F = params.vazao
+  V = params.volume
+  k = params.constante
+  CA = F/(F+V*k)*CAin
+  return CA
+
+def simula_cstr(CA0 = 1.0, num_estagios = 3, params):
+  CAin = CA0
+  CA = np.zeros(num_estagios)
+  X = np.zeros(num_estagios)
+  for j in range(num_estagios):
+    CA[j] = modelo_cstr(CAin, params[j])
+    X[j] = (CA0-CA[j])/CA0
+    CAin = CA[j]
+  return CA, X
+    
